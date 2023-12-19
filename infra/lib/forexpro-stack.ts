6cbf55302bff68e344fdecbf6a9cc4b351bb4e3e
Matheus Muniz <matheusmuniz215@gmail.com>
2023-12-19 10:41:34 -0300

    feat: Dynamo table construct

--- infra/lib/forexpro-stack.ts
+++ infra/lib/forexpro-stack.ts
@@ -1,5 +1,6 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class ForexProStack extends Stack {
@@ -12,6 +13,14 @@ export class ForexProStack extends Stack {
       fifo: true,
       retentionPeriod: Duration.days(1),
       queueName: 'TradesQueue.fifo',
+    });
+
+    const user_trades_table = new Table(this, 'UserTradesTable', {
+      partitionKey: {
+        name: 'partition_key',
+        type: AttributeType.STRING,
+      },
+      sortKey: { name: 'sort_key', type: AttributeType.STRING },
     });
   }
 }
