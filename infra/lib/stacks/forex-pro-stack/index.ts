e1635b1bba9158586a7f47df1087a8d1e8bc5ec6
Matheus Muniz <matheusmuniz215@gmail.com>
2023-12-21 11:07:01 -0300

    feat: secrets manager configuration

--- infra/lib/stacks/forex-pro-stack/index.ts
+++ infra/lib/stacks/forex-pro-stack/index.ts
@@ -5,6 +5,7 @@ import { Construct } from 'constructs';
 /* ------------ Constructs ----------- */
 import { ForexProQueue } from '../../constructs/sqs';
 import { ForexProDynamoTable } from '../../constructs/dynamo';
+import { ForexProSecret } from '../../constructs/secrets-manager';
 
 /* ------------ Types ----------- */
 import { CDK } from '../../../@types/cdk';
@@ -12,11 +13,12 @@ import { CDK } from '../../../@types/cdk';
 export class ForexProStack extends Stack {
   public readonly sqs_queue: ForexProQueue;
   public readonly dynamo_table: ForexProDynamoTable;
+  public readonly secret: ForexProSecret;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const { dynamo_table_name, sqs_queue_name }: CDK.Context =
+    const { dynamo_table_name, sqs_queue_name, secret_name }: CDK.Context =
       this.node.tryGetContext('ForexPro');
 
     /* ------------ Constructs ----------- */
@@ -26,6 +28,10 @@ export class ForexProStack extends Stack {
 
     this.dynamo_table = new ForexProDynamoTable(this, 'ForexProDynamoTable', {
       dynamo_table_name,
+    });
+
+    this.secret = new ForexProSecret(this, 'ForexProSecret', {
+      secret_name,
     });
   }
 }
