bc6dc277fab2d81b2e4e381a1815d5f5b06bdfd7
Matheus <matheusmuniz215@gmail.com>
2025-04-24 21:51:14 -0300

    fix: API healthcheck hook

--- client/src/lib/hooks/useAPIHealthCheck.tsx
+++ client/src/lib/hooks/useAPIHealthCheck.tsx
@@ -1,49 +1,24 @@
 import { useEffect } from 'react';
 import api from '../config/axios';
 
-const INTERVAL_DELAY = 3000;
-const MAX_RETRIES = 100;
-
 export const useAPIHealthCheck = () => {
   useEffect(() => {
     const controller = new AbortController();
-    let attempts = 0;
-    // eslint-disable-next-line prefer-const
-    let isAPIHealthy = false;
 
     const checkAPIHealth = async () => {
       if (controller.signal.aborted) return;
 
       try {
-        const { status, data } = await api.get('/healthcheck');
-
-        console.log({ status, data });
-
-        if (status === 200 && data === 'OK') isAPIHealthy = true;
+        await api.get('/healthcheck');
       } catch (e) {
-        console.error(e);
+        //
       }
-
-      attempts++;
     };
 
-    const interval = setInterval(() => {
-      if (
-        controller.signal.aborted ||
-        isAPIHealthy ||
-        attempts >= MAX_RETRIES
-      ) {
-        clearInterval(interval);
-        controller.abort();
-        return;
-      }
-
-      checkAPIHealth();
-    }, INTERVAL_DELAY);
+    checkAPIHealth();
 
     return () => {
       controller.abort();
-      clearInterval(interval);
     };
   }, []);
 };
