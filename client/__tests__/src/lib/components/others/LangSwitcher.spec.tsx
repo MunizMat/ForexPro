45bfab8e79843d81ef16a321d7579cd6efb1be82
Matheus <matheusmuniz215@gmail.com>
2025-05-10 21:21:28 -0300

    chore: remove frontend tests temporarily

--- client/__tests__/src/lib/components/others/LangSwitcher.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import LanguageSwitcher from '../../../../../src/lib/components/others/LangSwitcher';
-
-describe('Language Switcher', () => {
-  it('should render dropdown', () => {
-    render(<LanguageSwitcher locale="en-US" />);
-    expect(screen.getByText('US')).toBeInTheDocument();
-  });
-});
